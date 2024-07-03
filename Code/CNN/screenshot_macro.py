0dee1ff2679871d079451e9ecce6ff0be26d82a1
Jaden <jadendai123@gmail.com>
2024-07-02 20:07:57 -0400

    Update screenshot_macro.py

--- Code/CNN/screenshot_macro.py
+++ Code/CNN/screenshot_macro.py
@@ -12,7 +12,7 @@ def run_macro():
             #forward step a set number of times
             kb.press_and_release(forward_step)
             time.sleep(0.05)
-        # kb.press_and_release(screenshot)
+        kb.press_and_release(screenshot)
         time.sleep(1)
 
 def stop_macro():
@@ -26,14 +26,14 @@ screenshot = "t"
  
 #CAPTURE SETTINGS
 frames_interval = 50 #number of frames between screenshots
-level_length = 90 #level length in seconds, will be used to prevent the macro from running too long
+level_length = 10 #level length in seconds, will be used to prevent the macro from running too long
 fps = 30 #frames per second for the game
 
 n_screenshots = math.floor(level_length*fps/frames_interval) #number of screenshots to be taken
 
 print("Macro working. Press space to begin.")
 kb.wait('space')
-print("Macro activating in 5s. Tab into the level. Press space again to emergency stop.")
+print("Macro activating in 5s. Tab into the level.")
 time.sleep(5)
-kb.on_press_key("space", stop_macro())
+# kb.on_press_key("space", stop_macro())
 run_macro()
