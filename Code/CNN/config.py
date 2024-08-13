5606c749bd65711a1894504f7c181e831cfc1806
Jaden <jadendai123@gmail.com>
2024-08-12 20:44:22 -0400

    fixed the baseline model and updated classes

--- Code/CNN/config.py
+++ Code/CNN/config.py
@@ -12,8 +12,10 @@ class_colors_ = {'platform':     (000,000,187), # 0
                'blue jump orb':  (17,255,255), # 4
                'blue pad':       (7,102,102), # 5
                'yellow pad':     (102,102,27), # 6
-               'portal':         (17, 170, 0) #7 
-#              'background':     (000,000,000)  # 7 # Not needed as it is the default value
+               'pink jump orb':  (255,119,255), #7
+               'pink pad':       (102,48,102), #8
+               'portal':         (17, 170, 0) #9
+#              'background':     (000,000,000)  # 10 # Not needed as it is the default value
 }
 
 labels_ = class_colors_.keys()
